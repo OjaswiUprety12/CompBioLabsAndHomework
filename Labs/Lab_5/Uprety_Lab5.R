0c8b733a8284426d6d6e7e9191d68bfd87fb21d8
OjaswiUprety12 <ojup1319@colorado.edu>
2020-02-26 17:26:29 -0700

    Adding indentation and comments

--- Labs/Lab_5/Uprety_Lab5.R
+++ Labs/Lab_5/Uprety_Lab5.R
@@ -1,55 +1,56 @@
-#Lab 5 Loops and Conditionals 
-#Practice simple conditionals; practicing if and else statements 
+  #Lab 5 Loops and Conditionals 
+
+  #Practiceing simple conditionals; practicing if and else statements 
 x <- 4
 if (x > 5) {
   print("True")
 } else { 
   print("false")
 }
-#convert the data vector into a dataframe 
+
+  #Converting the data vector into a dataframe 
 y <- read.csv("/Users/ojaswiuprety/Desktop/EBIO4420/CompBioLabsAndHomework/Labs/Lab_5/ExampleData.csv")
 myVec <- y$x
-#Using a forloop to check each value in the imported data 
+
+  #Using a for loop to check each value in the imported data 
 for (i in 1:length(myVec)) {
   if (myVec[i] < 0) { 
    myVec[i] <- NA
   }
 }
-#Using vectorized code that makes use of "logical" indexing
+
+  #Using vectorized code that makes use of logical indexing
 ISNA <- is.na(myVec)
 myVec[ISNA] <- NaN
 
-# Using a which() statement and integer indexing 
+  #Using a which() statement and integer indexing 
 ISNA <- is.na(myVec)
 myVec[ISNA] <- NaN
 myThreshhold <- 0 
 myVec[ISNA] <- myThreshhold
 
-#Determining how many of the values fall in the range between 50 and 100 
+  #Determining how many of the values fall in the range between 50 and 100? 
 myVec >= 50 & myVec <= 100
 
-#Creating a new vector of data that fall in the range between 50 and 100 
-
+  #Creating a new vector of data that falls in the range between 50 and 100 
 y <- read.csv("/Users/ojaswiuprety/Desktop/EBIO4420/CompBioLabsAndHomework/Labs/Lab_5/ExampleData.csv")
 FiftyToOneHundred <- y$x
 FiftyToOneHundred <- subset(myVec, FiftyToOneHundred >= 50 & FiftyToOneHundred <= 100)
 
-#Save the newly made vector 
-
+  #Saveing the newly made vector 
 write.csv(x= FiftyToOneHundred, file="FiftyToOneHundred.csv")
 
-#Import CSV file for CO2 data 
+  #Importing the CSV file for CO2 data 
 setwd("/Users/ojaswiuprety/Desktop/EBIO4420/CompBioLabsAndHomework/Labs/Lab_5/")
 CO2Data <- read.csv("CO2_data_cut_paste.csv")
 
-#Wihtout using a loop: Answer "What was the first year for which data on "Gas" emissions were non zero?
+  #Wihtout using a loop: Answer "What was the first year for which data on "Gas" emissions were non zero?
 FirstYearEmissionsWereNonZero <- (CO2Data$Year[CO2Data$Gas > 0 ][1])
 
-#Answer: During which years were "Total" emissions between 200 and 300 million metric tons of carbon?
- EmissionsBetween200And300Million <- (CO2Data$Year[CO2Data$Total >= 200 & CO2Data$Total <= 300])
+  #Answer: During which years were "Total" emissions between 200 and 300 million metric tons of carbon?
+EmissionsBetween200And300Million <- (CO2Data$Year[CO2Data$Total >= 200 & CO2Data$Total <= 300])
 
-#Practicing Loops + Conditionals + Biology; Write a code that calculates the abundances of predators and prey over time according to this model 
-
+  #Practicing Loops + Conditionals + Biology; Write a code that calculates the abundances of predators and prey over time according to this model 
 totalGenerations <- 1000 
 initPrey <- 100 
 initPred <- 10 
@@ -58,27 +59,30 @@ r <- 0.2
 m <- 0.05 
 k <- 0.1
 
-#Create a time vector and 2 additional vectors for n over time and to store values of p 
+  #Create a time vector and 2 additional vectors for n over time and to store values of p 
 Time <- c(1:totalGenerations)
 n <- rep(initPrey, totalGenerations)
 p <- rep(initPred, totalGenerations)
 
-
-#Create a loop that implements the calculations 
+  #Create a loop that implements the calculations 
 for(t in 2:totalGenerations) {
   n[t] <- n[t-1] + (r * n[t-1]) - (a * n[t-1] * p[t-1])
   p[t] <- p[t-1] + (k * a * n[t-1] * p[t-1]) - (m * p[t-1])
+  
   #Adding some if statements to check for negative numbers each generation
-  if(n[t] <0 ) {
+if(n[t] < 0 ) {
     n[t] <- 0
+  }
+if(p[t] < 0) {
+    p[t] <- 0
   }
 }
 
-#Create a plot of the abundances of prey and predators over time 
+  #Create a plot of the abundances of prey and predators over time 
 plot(Time,n)
 lines(Time,n)
 
-#Create a matrix of the results 
+  #Create a matrix of the results 
 myResults <- cbind(Time, n, p) 
 myResults
 write.csv(x=myResults, file="myResults.csv" )
