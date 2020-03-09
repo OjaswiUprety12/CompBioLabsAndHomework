94322dbb2c7bbe502ed9ed2fbf986cf4f52614c4
OjaswiUprety12 <ojup1319@colorado.edu>
2020-03-09 16:25:13 -0600

    Updating lab number 8

--- Labs/Lab_8/Uprety_Lab8.R
+++ Labs/Lab_8/Uprety_Lab8.R
@@ -1,9 +1,36 @@
 #Lab 08 Documentation and Metadata 
+
 #Implementing the discrete-time logistic growth model 
+n <- 2500
+K <- 10000
+r <- 0.8 
+time <- c(1:12)
+for (t in seq(2, 12)) {
+  n[t] <- n[t-1] + (r * n[t-1] * (K - n[t-1]) / K)
+  print (n[t])
+}
+plot(time,n)
 
 #Turn this code into a function that takes r, K, the total number of generations, and the initial population size as its arguments.
+TimeLogisticGrowthModel <- function(r,K,n,NumberOfGen) {
+  time <- c(1:NumberOfGen)
+  for (t in seq(2, NumberOfGen)) {
+    n[t] <- n[t-1] + (r * n[t-1] * (K - n[t-1]) / K)
+    print (n[t])
+  }
+}
+#Testing this function with new values 
+TimeLogisticGrowthModel(1,3000,400,10)
 
 #Add code to this function so it produces a plot of the data it generates. 
+TimeLogisticGrowthModel <- function(r,K,n,NumberOfGen) {
+  time <- c(1:NumberOfGen)
+  for (t in seq(2, NumberOfGen)) {
+    n[t] <- n[t-1] + (r * n[t-1] * (K - n[t-1]) / K)
+    print (n[t])
+  }
+}
+TimeLogisticGrowthModel(1,4000,12,13)
 
 #Write code that calls this function 
 
